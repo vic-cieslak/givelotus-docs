1f471189b5c3e0392aa3a1c0212cca0e6ddd1ece
Vic <wiktorcie@gmail.com>
2022-01-07 16:28:40 +0100

    Working favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,5 +3,10 @@ import theme from '@nuxt/content-theme-docs'
 export default theme({
   docs: {
     primaryColor: '#E24F55'
+  },
+  head: {
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
   }
 })
